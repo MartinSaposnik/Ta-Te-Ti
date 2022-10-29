9308c2c0d29b82e58af2bfb65e0f199893b5bb6e
MartinSaposnik <martinrobertosaposnik@gmail.com>
2022-10-29 11:42:33 -0300

    new commit

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,27 +1,30 @@
 import Square from "../Square/Square";
 import './Board.css';
 
-const Board = ({squares}) => {
+const Board = ({ squares, onClick, turn, winningSquares }) => {
 
     const createSquares = values => (
         values.map(value => (
-            <Square 
-            value={squares[value]}
-            key={`square_${value}`}
+            <Square
+                winner={winningSquares.includes(value)}
+                turn={turn}
+                onClick={() => onClick(value)}
+                value={squares[value]}
+                key={`square_${value}`}
             />
         ))
     );
 
-    return(
+    return (
         <div className="board">
             <div className="row">
-                {createSquares([0,1,2])}
+                {createSquares([0, 1, 2])}
             </div>
             <div className="row">
-                {createSquares([3,4,5])}
+                {createSquares([3, 4, 5])}
             </div>
             <div className="row">
-                {createSquares([6,7,8])}
+                {createSquares([6, 7, 8])}
             </div>
         </div>
     );
--- src/components/Square/Square.js
+++ src/components/Square/Square.js
@@ -1,8 +1,20 @@
 import './Square.css';
+import classNames from 'classnames';
 
-const Square = ({value}) => {
-    return(
-        <div className="square">
+const Square = ({ value, onClick, turn, winner }) => {
+
+    const handleClick = () => {
+        (turn !== null && value === null) && onClick();
+    }
+
+    let squareClass = classNames({
+        square: true,
+        [`square--${value}`]: value !== null,
+        winner: winner,
+    });
+
+    return (
+        <div className={squareClass} onClick={() => handleClick()}>
 
         </div>
     )
